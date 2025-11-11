7186a1865b408c632c0a47a5dee117f918a2cd51
Armand <arma.asz@proton.me>
2025-11-11 21:56:59 +0100

    feat: update personal information in metadata and footer with correct name and details

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="py-10">
       <div className="self-stretch text-center justify-start text-zinc-400 text-base font-normal ">
-        Copyright © 2025 [Your Name]
+        Copyright © 2025 Armand Patrick Asztalos
         <br />
         All rights reserved.
       </div>
